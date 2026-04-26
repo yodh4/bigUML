6ddc820aba53b5b8935e561a4077a0193390f378
yodh4 <cyrilus2004@gmail.com>
2026-04-26 23:32:15 +0700

    fix(server): render package-owned relationships as siblings to prevent edge occlusion

--- server/app/src/main/java/com/borkdominik/big/glsp/uml/uml/elements/package_/gmodel/PackageGModelMapper.java
+++ server/app/src/main/java/com/borkdominik/big/glsp/uml/uml/elements/package_/gmodel/PackageGModelMapper.java
@@ -11,13 +11,17 @@
 package com.borkdominik.big.glsp.uml.uml.elements.package_.gmodel;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.eclipse.emf.common.util.Enumerator;
 import org.eclipse.glsp.graph.GModelElement;
 import org.eclipse.glsp.graph.GNode;
 import org.eclipse.uml2.uml.Package;
+import org.eclipse.uml2.uml.PackageableElement;
+import org.eclipse.uml2.uml.Relationship;
 
 import com.borkdominik.big.glsp.server.core.model.BGTypeProvider;
 import com.borkdominik.big.glsp.server.elements.gmodel.BGEMFElementGModelMapper;
@@ -45,8 +49,36 @@ public final class PackageGModelMapper extends BGEMFElementGModelMapper<Package,
       siblings.addAll(mapHandler.handle(source.getElementImports()));
       siblings.addAll(mapHandler.handle(source.getPackageImports()));
       siblings.addAll(mapHandler.handle(source.getPackageMerges()));
+      siblings.addAll(mapHandler.handle(packageOwnedRelationships(source)));
 
-      return siblings;
+      return deduplicateById(siblings);
+   }
+
+   protected List<PackageableElement> packageOwnedRelationships(final Package source) {
+      return source.getPackagedElements().stream()
+         .filter(this::isPackageOwnedRelationship)
+         .collect(Collectors.toList());
+   }
+
+   protected boolean isPackageOwnedRelationship(final PackageableElement element) {
+      return element instanceof Relationship;
+   }
+
+   protected List<GModelElement> deduplicateById(final List<GModelElement> elements) {
+      var withIds = new LinkedHashSet<String>();
+      var deduplicated = new ArrayList<GModelElement>();
+      for (var element : elements) {
+         var id = element.getId();
+         if (id == null) {
+            deduplicated.add(element);
+            continue;
+         }
+
+         if (withIds.add(id)) {
+            deduplicated.add(element);
+         }
+      }
+      return deduplicated;
    }
 
 }
